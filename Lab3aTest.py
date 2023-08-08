755514f6c779a8ae8d4d80fa4caf409c60eb73b1
Izzy Mansurov <130691832+csta01@users.noreply.github.com>
2023-08-08 16:23:14 -0600

    Update Lab3aTest.py

--- Lab3aTest.py
+++ Lab3aTest.py
@@ -2,13 +2,11 @@ import os
 import subprocess
 
 def run_job(cmd, input_data=None):
-    current_directory = os.getcwd()
     ret = subprocess.run(
         cmd,
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
-        cwd=current_directory,
         input=input_data,
         text=True,
     )
@@ -21,17 +19,20 @@ def remove_class_files():
 
 if __name__ == "__main__":
     # Compile
-    current_dir = os.getcwd()
-    src_dir = os.path.join(current_dir, "src")
-    os.chdir(src_dir)
+    src_dir = os.getcwd()  # Assuming you are in the directory containing Main.java
     compile_command = "javac Main.java"
     compile_result = run_job(compile_command)
     if compile_result:
         print(f"Compilation error:\n{compile_result}")
     else:
-        run_command = "java -cp . DessertShop"
-        run_result = run_job(run_command)
-        print("Your code works")
+        input_data = "41\n"  # Providing the input as 41
+        run_command = "java -cp . Main"  # Running the Main class
+        run_result = run_job(run_command, input_data=input_data)
+        expected_output = "A customer with a chest measurement of 41 inches needs to order a size L.\n"
+        if run_result.strip() == expected_output.strip():
+            print("Your code works")
+        else:
+            print(f"Unexpected output:\n{run_result}")
 
     # Remove .class files
     remove_class_files()
